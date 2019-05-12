a4675d52916ac229861760eb6fdec204a70498b2
Lyra <git@sblume.eu>
2019-05-12 11:53:44 +0200

    Minor: Let Resharper format the new project

--- Vidly/Models/ErrorViewModel.cs
+++ Vidly/Models/ErrorViewModel.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace Vidly.Models
 {
     public class ErrorViewModel
--- Vidly/Startup.cs
+++ Vidly/Startup.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -55,8 +50,8 @@ namespace Vidly
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    "default",
+                    "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
